5a44f3b57ad9fcaacbecac835c9525a0455c3db2
username <wecp-dummy@gmail.com>
2024-02-17 12:30:03 +0000

    wecp initial commit

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,43 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { AuthInterceptor } from "./auth.interceptors";
+
+
+const routes: Routes = [
+  {
+    path: "auth",
+    loadChildren: () => import("./auth/auth.module").then((m) => m.AuthModule),
+  },
+
+  {
+    path: "bank",
+    loadChildren: () => import("./bank/bank.module").then((m) => m.BankModule),
+  },
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "/auth",
+  },
+  // {
+  //   path:'testing',
+  //   component:TestingComponent
+  // }
+];
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [RouterModule.forRoot(routes), BrowserModule, HttpClientModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
+  exports: [RouterModule],
+  bootstrap: [AppComponent]
+})
+export class AppRoutingModule { }
