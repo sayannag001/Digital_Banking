a3f65c1e0f124d0978ce3526fa263a261d481585
username <wecp-dummy@gmail.com>
2024-02-17 06:22:51 +0000

    wecp initial commit

--- frontend/src/app/bank/components/dashboard/dashboard.component.ts
+++ frontend/src/app/bank/components/dashboard/dashboard.component.ts
@@ -42,7 +42,59 @@ export class DashboardComponent implements OnInit {
   
   }
 
-  
+  deteteCustomer(customer:any):void{
+   // alert(customer.customerId);
+    let conf = confirm("Do You Really Want To Delete Customer");
+    if(conf)
+    {
+      this.bankService.deleteCustomer(customer.customerId).subscribe(
+        () => {
+          alert('Customer deleted successfully.');
+
+          // Remove the deleted customer from the local array
+         this.customers$ = this.bankService.getCustomers();
+        },
+        (error) => {
+          console.error('Error deleting customer:', error);
+          // Handle error, show a message, etc.
+        }
+      );
+    }
+  }
+
+  editCustomer(customer:Customer):void{
+    this.router.navigate(['/bank/customer/edit', { customerId: customer.customerId,name:customer.name,email:customer.email,username:customer.username, password:customer.password,role:customer.role }]);
+
+  }
+
+  deteteAccount(account:any):void{
+    // alert(customer.customerId);
+     let conf = confirm("Do You Really Want To Delete Customer");
+     if(conf)
+     {
+       this.bankService.deleteAccount(account.accountId).subscribe(
+         () => {
+           alert('Account deleted successfully.');
  
+           // Remove the deleted customer from the local array
+          this.accounts$ = this.bankService.getAccounts();
+         },
+         (error) => {
+           console.error('Error deleting customer:', error);
+           // Handle error, show a message, etc.
+         }
+       );
+     }
+   }
+ 
+   
+   editAccount(account:any):void{
+    console.log(account);
+     this.router.navigate(['/bank/account/edit', { accountId: account.accountId,balance:account.balance,customerId:account.customer.customerId,
+      name:account.customer.name,username:account.customer.username,password:account.customer.pasword,email:account.customer.email,role:account.customer.role
+    }]);
+ 
+     //return null;
+   }
 
 }
